c518d0bb7a768e8386c8e4caf57a34f3e4dc123c
Jay <jaypatel41120@gmail.com>
2023-08-21 21:47:37 +0530

    Change Username Field in Database.cs File

--- SlotPOS/Utils/Database.cs
+++ SlotPOS/Utils/Database.cs
@@ -8,6 +8,6 @@ namespace SlotPOS.Utils
 {
     internal class Database
     {
-        public string connString = "datasource=192.168.1.254;port=3306;username=slotposuser;password=Slot@POSUSA;database=slotpayout";
+        public string connString = "datasource=192.168.1.254;port=3306;username=slotposuser@192.168.1.254;password=Slot@POSUSA;database=slotpayout";
     }
 }
