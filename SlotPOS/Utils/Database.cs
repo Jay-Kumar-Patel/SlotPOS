076f6fd4840f47efb7e2f4ecd4db6f3865b3caed
Jay <jaypatel41120@gmail.com>
2023-08-21 20:29:48 +0530

    Change in DataSource Field in Database.cs File

--- SlotPOS/Utils/Database.cs
+++ SlotPOS/Utils/Database.cs
@@ -8,6 +8,6 @@ namespace SlotPOS.Utils
 {
     internal class Database
     {
-        public string connString = "datasource=127.0.0.1;port=3306;username=slotposuser;password=Slot@POSUSA;database=slotpayout";
+        public string connString = "datasource=192.168.1.254;port=3306;username=slotposuser;password=Slot@POSUSA;database=slotpayout";
     }
 }
