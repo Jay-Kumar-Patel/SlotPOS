0e14d7c17d8df96665f5f04512a0d87b4e09fc46
Sarthak Patel <19a067@nirmauni.ac.in>
2023-08-28 00:18:31 -0700

    Shift Done

--- SlotPOS/Utils/Database.cs
+++ SlotPOS/Utils/Database.cs
@@ -9,6 +9,6 @@ namespace SlotPOS.Utils
     internal class Database
     {
         //192.168.1.254
-        public string connString = "datasource=127.0.0.1;port=3306;username=slotposuser;password=Slot@POSUSA;database=slotpayout";
+        public string connString = "datasource=192.168.1.254;port=3306;username=slotposuser;password=Slot@POSUSA;database=slotpayout";
     }
 }
